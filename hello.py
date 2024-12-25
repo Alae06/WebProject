b720d293f03d5ce30670eea5b4a5a91b0aa64ab3
ALAE <ALAE.MAJJATI.ESTN24@ump.ac.ma>
2024-12-25 19:01:08 +0100

    hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ from tkinter import messagebox
 class SnackStand:
     def __init__(self, root):
         self.root = root
-        self.root.title("Snack Stand")
+        self.root.title("Snack")
         self.root.geometry("800x600")
 
         self.menu = {
